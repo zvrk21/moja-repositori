6c82e558300a4f2961461e507a0d6a48ee21bdf8
zvrk21 <115645074+zvrk21@users.noreply.github.com>
2022-10-12 17:22:44 +0200

    Add files via upload

---
+++ dan2.js
@@ -0,0 +1,4 @@
+let a = 2.5e1;
+console.log(a);
+var p = "1s
+"
---
+++ script.js
@@ -0,0 +1,49 @@
+var broj = 5;
+var ime = "petar";
+var bul = true;
+var no = undefined;
+var p = null;
+
+console.log(bul);
+
+var igrac = 10;
+var ime = "fudbal";
+var lopta = true;
+var ofsajd = undefined;
+
+console.log(ime);
+console.log(ime);
+
+console.log((broj + igrac) / 2);
+
+console.log(3 - 2 - 31);
+console.log(3 * 5 - 7 / 3);
+console.log(4 / 2 - 1.14 * 3);
+
+var bananaKg = 1.2;
+var bananaGram = bananaKg * 1000;
+console.log(bananaGram + " gr");
+
+var anna = 5 * 60;
+var tom = 30 * 60;
+
+console.log("Anna is late " + anna + " seconds");
+console.log("Tom is late " + tom + " seconds");
+
+var stickGb = 4;
+var stickMb = stickGb * 1000;
+console.log(stickMb);
+
+var newsArticlesNumber = (stickMb * 1000) / 98;
+console.log(newsArticlesNumber);
+
+var a = 5;
+console.log((a = a * 2));
+
+console.log(typeof "number");
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof "false");
+console.log(typeof 56);
+
+console.log(true + true);
